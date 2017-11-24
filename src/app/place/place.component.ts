50825b823c30ed188ccd52e11355e6f669750641
estigmata <nestor.otondo@gmail.com>
2017-11-24 09:57:34 -0400

    Add carousel slider

--- src/app/place/place.component.ts
+++ src/app/place/place.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+
 import { PEOPLES } from './mock-list.people';
 
 @Component({
   selector: 'app-place',
   templateUrl: './place.component.html',
-  styleUrls: ['./place.component.css']
+  styleUrls: ['./place.component.css'],
+  providers: [ NgbCarouselConfig ]
 })
 export class PlaceComponent implements OnInit {
   allPeople = PEOPLES;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, config: NgbCarouselConfig) {
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+  }
 
   ngOnInit() {}
 
